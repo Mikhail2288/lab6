19347ed337054f79aed5b724e957f9f4da873e6b
Mikhail2288 <145277113+Mikhail2288@users.noreply.github.com>
2024-12-26 01:36:18 +0500

    Normal.java

---
+++ 2/6/Normal.java
@@ -0,0 +1,15 @@
+@Cache
+public class Normal {
+    private String name;
+    private int age;
+
+    public Normal(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Normal {name = '" + name + ", age=" + age +'}';
+    }
+}
